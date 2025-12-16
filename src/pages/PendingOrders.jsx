2f51915e39db2b7c5f4589ceacd044560283ddf1
clydejustin1117 <23-11498@g.batstate-u.edu.ph>
2025-12-15 21:08:07 -0800

    Fix import name

--- src/pages/PendingOrders.jsx
+++ src/pages/PendingOrders.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-import InventoryAlert from "./Inventory";
+import InventoryAlert from "./InventoryAlert";
 
 const PendingOrders =  ({ }) =>{
   const [orders, setOrders] = useState([]);
